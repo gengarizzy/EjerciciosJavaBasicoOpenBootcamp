247fcb0d5ad9fc699fdd271b4bb1387f4e369c77
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-01-02 20:43:38 -0300

    Ejercicio Tema 1 JavaBasico

---
+++ src/EjercicioTema1JAVAOB.java
@@ -0,0 +1,30 @@
+public class EjercicioTema1JAVAOB {
+
+    public static void main(String[] args) {
+
+        // int, long, double, boolean, String.
+
+        int datoint = 5;
+
+        long datolong = 3;
+
+        double datodouble = 2.3;
+
+        boolean datoboolean = false;
+
+        String datostring = "Este es el contenido del String datostring";
+
+
+        System.out.println(datoint + " --> imprimo un dato de tipo int");
+
+        System.out.println(datolong + " --> imprimo un dato de tipo long");
+
+        System.out.println(datodouble + " --> imprimo un dato de tipo double");
+
+        System.out.println(datoboolean + " --> imprimo un dato de tipo boolean");
+
+        System.out.println(datostring + " --> imprimo un dato de tipo String");
+
+
+    }
+}
