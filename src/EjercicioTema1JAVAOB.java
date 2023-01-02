f2126d28158c0c44617f72eb71f96d233224cc53
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-01-02 20:56:53 -0300

    Update EjercicioTema1JAVAOB.java

--- src/EjercicioTema1JAVAOB.java
+++ src/EjercicioTema1JAVAOB.java
@@ -4,26 +4,48 @@ public class EjercicioTema1JAVAOB {
 
         // int, long, double, boolean, String.
 
+        byte datobyte = 0;
+
         int datoint = 5;
+
+        short datoshort = 0;
 
         long datolong = 3;
 
         double datodouble = 2.3;
 
-        boolean datoboolean = false;
+        float datofloat = 1.2;
+
+        boolean datobooleanfalse = false;
+
+        boolean datobooleantrue = true;
+
+        char datochar = 'u';
 
         String datostring = "Este es el contenido del String datostring";
 
+    // RESOLUCION
+
+
+        System.out.println(datobyte + " --> imprimo un dato de tipo byte");
 
         System.out.println(datoint + " --> imprimo un dato de tipo int");
+
+        System.out.println(datoshort + " --> imprimo un dato de tipo short");
 
         System.out.println(datolong + " --> imprimo un dato de tipo long");
 
         System.out.println(datodouble + " --> imprimo un dato de tipo double");
 
-        System.out.println(datoboolean + " --> imprimo un dato de tipo boolean");
+        System.out.println(datofloat + " --> imprimo un dato de tipo float");
+
+        System.out.println(datobooleanfalse + " --> imprimo un dato de tipo boolean falso");
+
+        System.out.println(datobooleantrue + " --> imprimo un dato de tipo boolean verdadero");
 
         System.out.println(datostring + " --> imprimo un dato de tipo String");
+
+        System.out.println(datochar + " --> imprimo un dato de tipo char");
 
 
     }
